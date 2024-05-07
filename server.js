ef7a390f316d33e30b3b0eee4a381682606f2581
Mayanke <mayankthakur1294@gmail.com>
2024-05-07 16:45:26 +0530

    updated the backend

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const cors= require("cors");
 const { GoogleGenerativeAI } = require("@google/generative-ai"); // Assuming this library exists
 
 
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -14,31 +13,13 @@ const port = process.env.PORT || 3000; // Use environment variable for port or d
 const API_KEY = process.env.API_KEY;
 
 
-
-// const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-// Access your API key as an environment variable (see "Set up your API key" above)
-// const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-
-// async function run() {
-//   // For text-only input, use the gemini-pro model
-//   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-
-//   const prompt = "Write a story about a magic backpack."
-
-//   const result = await model.generateContent(prompt);
-//   const response = await result.response;
-//   const text = response.text();
-//   console.log(text);
-// }
-
-// run();
+app.get("/",(req,res)=>{
+  res.send("Welcome to Shayari Generator Backend")
+})
 
   // Function to generate Shayari
   async function generateShayari(keyword, genAI) {
     try {
-      
-      
 
         // Get the Gemini Pro model
         const model = genAI.getGenerativeModel({ model: "gemini-pro"});
@@ -54,15 +35,14 @@ const API_KEY = process.env.API_KEY;
         const shayari = response.text() || 'Could not generate Shayari at this time.';
 // console.log(shayari)
         return shayari;
-    } catch (error) {x
+    } catch (error) {
       if ( error.message.includes('SAFETY')) {
         console.log('Shayari generation failed due to safety concerns,try with a different keyword:', error);
         return 'Shayari generation failed due to safety concern. Please try a different keyword.';
       }else{
         console.error('Error generating Shayari:', error);
       }
-        
-       
+          
     }
 }
 
@@ -76,20 +56,12 @@ app.post('/generate-shayari', async (req, res) => {
             
           }
           const genAI = new GoogleGenerativeAI(API_KEY);
-        const keyword = req.body.keyword;
-        
-
-        // Validate keyword (optional)
+          const keyword = req.body.keyword;
 
         // Check if API key is set in environment variable
         if (!API_KEY) {
             return res.status(401).json({ message: 'Missing GEMINI_API_KEY environment variable' });
         }
-
-        // Create GoogleGenerativeAI instance with API key
-       
-
-      
 
         // Generate Shayari and send response
         const shayari = await generateShayari(keyword, genAI);
@@ -99,7 +71,6 @@ app.post('/generate-shayari', async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 });
-
 
 
 // Start the server
